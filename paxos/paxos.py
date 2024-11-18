5c6caa6b7378028e1ab5f6d9ab537cf52f638af3
alessandroCassani <acassani@studenti.uninsubria.it>
2024-11-18 11:25:13 +0100

    multipaxos with separate classe, but test3 bugged

--- paxos/paxos.py
+++ paxos/paxos.py
@@ -3,10 +3,11 @@ import sys
 import socket
 import struct
 import time
-from collections import defaultdict
 import threading
 import json
 import math
+from collections import defaultdict
+
 
 class Message:
     PHASE1A = "PHASE_1A"
@@ -17,18 +18,17 @@ class Message:
 
     @staticmethod
     def prepare(c_rnd, instance_id, timestamp):
-        msg = {
+        return json.dumps({
             "phase": Message.PHASE1A,
             "c_rnd_1": c_rnd[0],
             "c_rnd_2": c_rnd[1],
             "instance": instance_id,
             "timestamp": timestamp
-        }
-        return json.dumps(msg).encode()
+        }).encode()
 
     @staticmethod
     def promise(rnd, v_rnd, v_val, instance_id, timestamp):
-        msg = {
+        return json.dumps({
             "phase": Message.PHASE1B,
             "rnd_1": rnd[0],
             "rnd_2": rnd[1],
@@ -37,44 +37,41 @@ class Message:
             "v_val": v_val,
             "instance": instance_id,
             "timestamp": timestamp
-        }
-        return json.dumps(msg).encode()
+        }).encode()
 
     @staticmethod
     def accept(c_rnd, c_val, instance_id, timestamp):
-        msg = {
+        return json.dumps({
             "phase": Message.PHASE2A,
             "c_rnd_1": c_rnd[0],
             "c_rnd_2": c_rnd[1],
             "c_val": c_val,
             "instance": instance_id,
             "timestamp": timestamp
-        }
-        return json.dumps(msg).encode()
+        }).encode()
 
     @staticmethod
     def decide(v_rnd, v_val, instance_id, timestamp):
-        msg = {
+        return json.dumps({
             "phase": Message.PHASE2B,
             "v_rnd_1": v_rnd[0],
             "v_rnd_2": v_rnd[1],
             "v_val": v_val,
             "instance": instance_id,
             "timestamp": timestamp
-        }
-        return json.dumps(msg).encode()
+        }).encode()
 
     @staticmethod
     def client_value(value, timestamp):
-        msg = {
+        return json.dumps({
             "phase": Message.CLIENT,
             "value": value,
             "timestamp": timestamp
-        }
-        return json.dumps(msg).encode()
+        }).encode()
 
-# Network functions remain the same
+
 def mcast_receiver(hostport):
+    """Create a multicast socket listening to the address"""
     recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     recv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     recv_sock.bind(hostport)
@@ -83,9 +80,11 @@ def mcast_receiver(hostport):
     return recv_sock
 
 def mcast_sender():
+    """Create a UDP socket"""
     return socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
 def parse_cfg(cfgpath):
+    """Parse configuration file"""
     cfg = {}
     with open(cfgpath, "r") as cfgfile:
         for line in cfgfile:
@@ -93,206 +92,278 @@ def parse_cfg(cfgpath):
             cfg[role] = (host, int(port))
     return cfg
 
+"""
+State Classes
+------------
+Classes for managing state of different Paxos roles
+"""
+class InstanceState:
+    def __init__(self):
+        self.c_rnd = None
+        self.c_val = None
+        self.timestamp = None
+        self.promises = []
+        self.is_decided = False
+        self.pending_timer = None
+
+class AcceptorState:
+    def __init__(self, id):
+        self.rnd = (0, id)
+        self.v_rnd = None
+        self.v_val = None
+        self.timestamp = None
+
+class ProposerState:
+    def __init__(self, id):
+        self.id = id
+        self.c_rnd_counter = (0, id)
+        self.current_instance = 0
+        self.instances = {}  # instance_id -> InstanceState
+        self.decided_values = set()
+        self.pending_values = []  # list of values sent by clients
+
+    def increment_round(self):
+        self.c_rnd_counter = (self.c_rnd_counter[0] + 1, self.c_rnd_counter[1])
+        return self.c_rnd_counter
+
+
+lock = threading.Lock()
+active_acceptors = set()
+
 def get_quorum():
     if len(active_acceptors) < 2:
         return 0
     else:
         return math.ceil(len(active_acceptors) / 2)
 
-lock = threading.Lock()
-active_acceptors = set()
-
 def resend_prepare(instance_id, prepare_msg, acceptors, sender):
     sender.sendto(prepare_msg, acceptors)
     print(f"Resending prepare message for instance {instance_id}")
 
-def acceptor(config, id):   
-    print("-> acceptor", id)
-    instances = defaultdict(lambda: {"rnd": (0, id), "v_rnd": None, "v_val": None, "timestamp": None})
+"""
+Paxos Roles
+-----------
+Implementation of each Paxos role
+"""
+class PaxosAcceptor:
+    """Acceptor role implementation"""
+    def __init__(self, config, id):
+        self.config = config
+        self.id = id
+        self.instances = defaultdict(lambda: AcceptorState(id))
+        self.sender = mcast_sender()
+
+    def handle_prepare(self, msg):
+        instance_id = msg["instance"]
+        timestamp = msg["timestamp"]
+        c_rnd = (msg["c_rnd_1"], msg["c_rnd_2"])
+        
+        instance = self.instances[instance_id]
+        if c_rnd > instance.rnd:
+            instance.rnd = c_rnd
+            instance.timestamp = timestamp
+            promise = Message.promise(
+                instance.rnd,
+                instance.v_rnd,
+                instance.v_val,
+                instance_id,
+                timestamp
+            )
+            self.sender.sendto(promise, self.config["proposers"])
+
+    def handle_accept(self, msg):
+        instance_id = msg["instance"]
+        timestamp = msg["timestamp"]
+        c_rnd = (msg["c_rnd_1"], msg["c_rnd_2"])
+        
+        instance = self.instances[instance_id]
+        if c_rnd >= instance.rnd:
+            instance.v_rnd = c_rnd
+            instance.v_val = msg["c_val"]
+            instance.timestamp = timestamp
+            
+            decide_msg = Message.decide(
+                instance.v_rnd,
+                instance.v_val,
+                instance_id,
+                timestamp
+            )
+            self.sender.sendto(decide_msg, self.config["learners"])    
+            self.sender.sendto(decide_msg, self.config["proposers"])
+
+class PaxosProposer:
+    """Proposer role implementation"""
+    def __init__(self, config, id):
+        self.config = config
+        self.state = ProposerState(id)
+        self.sender = mcast_sender()
+
+    def handle_client_message(self, msg):
+        self.state.pending_values.append((msg["timestamp"], msg["value"]))
+        
+        if (not self.state.instances or 
+            self.state.instances[self.state.current_instance].is_decided):
+            self.start_new_instance()
+
+    def start_new_instance(self):
+        if not self.state.pending_values:
+            return
+            
+        self.state.pending_values.sort(key=lambda x: x[0])
+        timestamp, value = self.state.pending_values.pop(0)
+        
+        if value in self.state.decided_values:
+            return
+            
+        instance = InstanceState()
+        instance.c_rnd = self.state.increment_round()
+        instance.c_val = value
+        instance.timestamp = timestamp
+        self.state.instances[self.state.current_instance] = instance
+        
+        prepare_msg = Message.prepare(
+            instance.c_rnd,
+            self.state.current_instance,
+            timestamp
+        )
+        instance.pending_timer = threading.Timer(
+            5.0, resend_prepare, 
+            args=(self.state.current_instance, prepare_msg, self.config["acceptors"], self.sender)
+        )
+        instance.pending_timer.start()
+        self.sender.sendto(prepare_msg, self.config["acceptors"])
+
+    def handle_promise(self, msg):
+        instance_id = msg["instance"]
+        if instance_id not in self.state.instances:
+            return
+            
+        instance = self.state.instances[instance_id]
+        rnd = (msg["rnd_1"], msg["rnd_2"])
+        
+        if rnd != instance.c_rnd:
+            return
+            
+        instance.promises.append(msg)
+        
+        if len(instance.promises) > get_quorum() and get_quorum() != 0:
+            self.handle_quorum_promises(instance, instance_id)
+
+    def handle_quorum_promises(self, instance, instance_id):
+        if any(p["v_rnd_1"] is not None and p["v_rnd_2"] is not None for p in instance.promises):
+            k = max(
+                (p["v_rnd_1"], p["v_rnd_2"]) 
+                for p in instance.promises 
+                if p["v_rnd_1"] is not None and p["v_rnd_2"] is not None
+            )
+            if k:
+                instance.c_val = next(
+                    p["v_val"] for p in instance.promises 
+                    if (p["v_rnd_1"], p["v_rnd_2"]) == k
+                )
+                    
+        accept_msg = Message.accept(
+            instance.c_rnd,
+            instance.c_val,
+            instance_id,
+            instance.timestamp
+        )
+        self.sender.sendto(accept_msg, self.config["acceptors"])
+        
+        instance.promises = []
+        if instance.pending_timer:
+            instance.pending_timer.cancel()
+            instance.pending_timer = None
+
+    def handle_decide(self, msg):
+        instance_id = msg["instance"]
+        if instance_id in self.state.instances:
+            instance = self.state.instances[instance_id]
+            instance.is_decided = True
+            self.state.decided_values.add(instance.c_val)
+            
+            if instance.pending_timer:
+                instance.pending_timer.cancel()
+                instance.pending_timer = None
+            
+            if self.state.pending_values:
+                self.state.current_instance += 1
+                self.start_new_instance()
+
+class PaxosLearner:
+    """Learner role implementation"""
+    def __init__(self):
+        self.decisions = []  # List of (timestamp, value) pairs
+        self.printed_up_to = 0  # Track the timestamps we've printed
     
+    def handle_decision(self, msg):
+        self.decisions.append((msg["timestamp"], msg["v_val"]))
+        self.decisions.sort(key=lambda x: x[0])
+        
+        while self.decisions and self.decisions[0][0] > self.printed_up_to:
+            timestamp, value = self.decisions.pop(0)
+            print(f"[{timestamp}] {value}")
+            sys.stdout.flush()
+            self.printed_up_to = timestamp
+
+#-------------------------------------------------------------------------------------
+
+def acceptor(config, id):
+    print("-> acceptor", id)
     with lock:
         active_acceptors.add(id)
-    r = mcast_receiver(config["acceptors"])
-    s = mcast_sender()
+    
+    receiver = mcast_receiver(config["acceptors"])
+    paxos = PaxosAcceptor(config, id)
     
     try:
         while True:
-            msg = json.loads(r.recv(2**16).decode())
-            instance_id = msg.get("instance")
-            timestamp = msg.get("timestamp")
-            
+            msg = json.loads(receiver.recv(2**16).decode())
             if msg["phase"] == Message.PHASE1A:
-                c_rnd = (msg["c_rnd_1"], msg["c_rnd_2"])
-                if c_rnd > instances[instance_id]["rnd"]:
-                    instances[instance_id]["rnd"] = c_rnd
-                    instances[instance_id]["timestamp"] = timestamp
-                    promise = Message.promise(
-                        instances[instance_id]["rnd"],
-                        instances[instance_id]["v_rnd"],
-                        instances[instance_id]["v_val"],
-                        instance_id,
-                        timestamp
-                    )
-                    s.sendto(promise, config["proposers"])
-                    
+                paxos.handle_prepare(msg)
             elif msg["phase"] == Message.PHASE2A:
-                c_rnd = (msg["c_rnd_1"], msg["c_rnd_2"])
-                if c_rnd >= instances[instance_id]["rnd"]:
-                    instances[instance_id]["v_rnd"] = c_rnd
-                    instances[instance_id]["v_val"] = msg["c_val"]
-                    instances[instance_id]["timestamp"] = timestamp
-                    
-                    decide_msg = Message.decide(
-                        instances[instance_id]["v_rnd"],
-                        instances[instance_id]["v_val"],
-                        instance_id,
-                        timestamp
-                    )
-                    s.sendto(decide_msg, config["learners"])    
-                    s.sendto(decide_msg, config["proposers"])
+                paxos.handle_accept(msg)
     finally:
         with lock:
             active_acceptors.remove(id)
 
 def proposer(config, id):
     print("-> proposer", id)
-    r = mcast_receiver(config["proposers"])
-    s = mcast_sender()
-    
-    c_rnd_cnt = (0, id)
-    current_instance = 0
-    instances = {}
-    decided_values = set()
-    pending_values = []  # Simple list of (timestamp, value) tuples
-    
-    def initialize_instance(instance_id):
-        if instance_id not in instances:
-            instances[instance_id] = {
-                "c_rnd": None,
-                "c_val": None,
-                "timestamp": None,
-                "promises": [],
-                "is_decided": False,
-                "pending_timer": None
-            }
-    
-    def start_new_instance():
-        nonlocal current_instance, c_rnd_cnt
-        if not pending_values:
-            return
-            
-        # Get the earliest timestamp value
-        pending_values.sort(key=lambda x: x[0])  # Sort by timestamp
-        timestamp, value = pending_values.pop(0)
-        
-        if value not in decided_values:
-            initialize_instance(current_instance)
-            c_rnd_cnt = (c_rnd_cnt[0] + 1, c_rnd_cnt[1])
-            instances[current_instance]["c_rnd"] = c_rnd_cnt
-            instances[current_instance]["c_val"] = value
-            instances[current_instance]["timestamp"] = timestamp
-            
-            prepare_msg = Message.prepare(c_rnd_cnt, current_instance, timestamp)
-            instances[current_instance]["pending_timer"] = threading.Timer(
-                5.0, resend_prepare, 
-                args=(current_instance, prepare_msg, config["acceptors"], s)
-            )
-            instances[current_instance]["pending_timer"].start()
-            s.sendto(prepare_msg, config["acceptors"])
+    receiver = mcast_receiver(config["proposers"])
+    paxos = PaxosProposer(config, id)
     
     while True:
-        msg = json.loads(r.recv(2**16).decode())
-        phase = msg["phase"]
-        
-        if phase == Message.CLIENT:
-            pending_values.append((msg["timestamp"], msg["value"]))
-            
-            # If no instance is active, start a new one
-            if not instances or instances[current_instance]["is_decided"]:
-                start_new_instance()
-        
-        elif phase == Message.PHASE1B:
-            instance_id = msg["instance"]
-            if instance_id not in instances:
-                continue
-                
-            inst = instances[instance_id]
-            rnd = (msg["rnd_1"], msg["rnd_2"])
-            
-            if rnd != inst["c_rnd"]:
-                continue
-                
-            inst["promises"].append(msg)
-            
-            if len(inst["promises"]) > get_quorum() and get_quorum() != 0:
-                if any(p["v_rnd_1"] is not None and p["v_rnd_2"] is not None for p in inst["promises"]):
-                    k = max(
-                        (p["v_rnd_1"], p["v_rnd_2"]) 
-                        for p in inst["promises"] 
-                        if p["v_rnd_1"] is not None and p["v_rnd_2"] is not None
-                    )
-                    if k:
-                        inst["c_val"] = next(
-                            p["v_val"] for p in inst["promises"] 
-                            if (p["v_rnd_1"], p["v_rnd_2"]) == k
-                        )
-                        
-                accept_msg = Message.accept(inst["c_rnd"], inst["c_val"], instance_id, inst["timestamp"])
-                s.sendto(accept_msg, config["acceptors"])
-                inst["promises"] = []
-                
-                if inst["pending_timer"]:
-                    inst["pending_timer"].cancel()
-                    inst["pending_timer"] = None
-        
-        elif phase == Message.PHASE2B:
-            instance_id = msg["instance"]
-            if instance_id in instances:
-                inst = instances[instance_id]
-                inst["is_decided"] = True
-                decided_values.add(inst["c_val"])
-                
-                if inst["pending_timer"]:
-                    inst["pending_timer"].cancel()
-                    inst["pending_timer"] = None
-                
-                # Move to next instance if there are more values to decide
-                if pending_values:
-                    current_instance += 1
-                    start_new_instance()
+        msg = json.loads(receiver.recv(2**16).decode())
+        if msg["phase"] == Message.CLIENT:
+            paxos.handle_client_message(msg)
+        elif msg["phase"] == Message.PHASE1B:
+            paxos.handle_promise(msg)
+        elif msg["phase"] == Message.PHASE2B:
+            paxos.handle_decide(msg)
 
 def learner(config, id):
     print("-> learner", id)
-    r = mcast_receiver(config["learners"])
-    decisions = []  # List of (timestamp, value) pairs
-    printed_up_to = 0  # Track the timestamps we've printed
+    receiver = mcast_receiver(config["learners"])
+    paxos = PaxosLearner()
     
     while True:
-        msg = json.loads(r.recv(2**16).decode())
+        msg = json.loads(receiver.recv(2**16).decode())
         if msg["phase"] == Message.PHASE2B:
-            decisions.append((msg["timestamp"], msg["v_val"]))
-            decisions.sort(key=lambda x: x[0])  # Sort by timestamp
-            
-            # Print any decisions that are ready
-            while decisions and decisions[0][0] > printed_up_to:
-                timestamp, value = decisions.pop(0)
-                print(f"[{timestamp}] {value}")
-                sys.stdout.flush()
-                printed_up_to = timestamp
+            paxos.handle_decision(msg)
 
 def client(config, id):
     print(f"-> client {id} starting")
-    s = mcast_sender()
+    sender = mcast_sender()
     
     for value in sys.stdin:
         value = value.strip()
-        current_time = time.time()
-        timestamp = int(current_time * 1_000_000)
+        timestamp = int(time.time() * 1_000_000)
         client_msg = Message.client_value(value, timestamp)
-        s.sendto(client_msg, config["proposers"])
+        sender.sendto(client_msg, config["proposers"])
         time.sleep(0.001)
     
     print(f"Client {id} finished")
+
 
 if __name__ == "__main__":
     cfgpath = sys.argv[1]
@@ -300,12 +371,15 @@ if __name__ == "__main__":
     role = sys.argv[2]
     id = int(sys.argv[3])
     
-    if role == "acceptor":
-        rolefunc = acceptor
-    elif role == "proposer":
-        rolefunc = proposer
-    elif role == "learner":
-        rolefunc = learner
-    elif role == "client":
-        rolefunc = client
-    rolefunc(config, id)
+    roles = {
+        "acceptor": acceptor,
+        "proposer": proposer,
+        "learner": learner,
+        "client": client
+    }
+    
+    if role not in roles:
+        print(f"Invalid role: {role}")
+        sys.exit(1)
+        
+    roles[role](config, id)
